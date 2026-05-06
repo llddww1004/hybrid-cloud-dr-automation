8cf5c1c4ceb72b3bc23bc4d4fe51a1664cf571e2
llddww1004 <skkse3669@gmail.com>
2026-05-06 09:48:18 +0900

    Delete project-springboot directory

--- project-springboot/modules/database/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "rds_endpoint" {
-  value = aws_db_instance.main.address
-}
-
-output "rds_port" {
-  value = aws_db_instance.main.port
-}
--- project-springboot/root/dr/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "3team-tfstate-dr-01"
-    key            = "project-springboot/dr/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
